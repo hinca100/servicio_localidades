748addfe1f8648b79773be6d6474cdf478036fe1
hinca120 <69090375+hinca120@users.noreply.github.com>
2020-08-02 19:55:26 -0500

    se agrega mas informacion a la base de datos

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,7 @@
 /* Populate tabla tareas */
 INSERT INTO tareas (nombre, descripcion, duracion) VALUES('mantenimiento celulares', 'arreglar los celulares','1 dia');
+INSERT INTO tareas (nombre, descripcion, duracion) VALUES('limpieza de equipos', 'mantenimientos preventivo','5 horas');
+INSERT INTO tareas (nombre, descripcion, duracion) VALUES('Actualizacion de equipos', 'actualizacion de software','3 horas');
+INSERT INTO tareas (nombre, descripcion, duracion) VALUES('Actualizacion de base de datos', 'registro de componentes y cambios de usuario','1 hora');
+INSERT INTO tareas (nombre, descripcion, duracion) VALUES('Registro de equipos nuevos', 'actualizar cualquier tipo de hadware nuevo ','1 hora');
+INSERT INTO tareas (nombre, descripcion, duracion) VALUES('Vaciar discos duros', 'borrar datos obsoletos del disco ','1 hora');
